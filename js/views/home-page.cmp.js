e1a5c755334622f3880137609caf77380e03a8e7
RoeeFurman <98423141+RoeeFurman@users.noreply.github.com>
2022-03-01 09:56:33 +0200

    Update home-page.cmp.js

--- js/views/home-page.cmp.js
+++ js/views/home-page.cmp.js
@@ -4,7 +4,7 @@ export default {
             <div class="welcome">Welcome to Miss-Book</div>
             <h1>Get your own Adventures</h1>
             <router-link to="/book" class="home-go">Click to start</router-link>
-            <img src="././img/books.png" class="home-img"/>
+            <img src="../../img/books.png" class="home-img"/>
         </section>
     `,
 }
